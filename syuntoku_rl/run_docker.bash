d2c52bede1b1e68a5860e6e88031163f4561522c
Borja Briso <borja.briso-montiano@aist.go.jp>
2019-08-04 19:49:38 +0900

    Support sacred with mongo_db

--- syuntoku_rl/run_docker.bash
+++ syuntoku_rl/run_docker.bash
@@ -1,10 +1,12 @@
 docker run -it --rm \
 	-p 6080:6080 \
 	-p 5000:5000 \
+	--net sacred_omniboard \
+	--link mongo_db \
 	-v /home/digital/RL:/root/RL \
 	-v /home/digital/rl_trainer:/root/rl_trainer \
 	-w /root \
 	--entrypoint=/bin/bash \
-	syuntoku/rl:chainer-latest
+	syuntoku/rl:sacred
 
 
--- syuntoku_rl/run_tensorboard.bash
+++ syuntoku_rl/run_tensorboard.bash
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-docker run --rm -it -v $1:/logs -p 6006:6006 tensorboard
+
+if [ $# -eq 0 ]
+then
+	echo "No directory path supplied"
+else
+	docker run --rm -it -v $1:/logs -p 6006:6006 syuntoku/tensorboard
+fi
