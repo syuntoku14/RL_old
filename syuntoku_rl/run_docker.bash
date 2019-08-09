3110f073ed8110038c5c13e0045117b543701f5e
Borja Briso <borja.briso-montiano@aist.go.jp>
2019-08-09 15:48:06 +0900

    Add sacred support

--- syuntoku_rl/run_docker.bash
+++ syuntoku_rl/run_docker.bash
@@ -1,12 +1,35 @@
-docker run -it --rm \
-	-p 6080:6080 \
-	-p 5000:5000 \
-	--net sacred_omniboard \
-	--link mongo_db \
-	-v /home/digital/RL:/root/RL \
-	-v /home/digital/rl_trainer:/root/rl_trainer \
-	-w /root \
-	--entrypoint=/bin/bash \
-	syuntoku/rl:sacred
+run_docker() {
+	docker run -it --rm \
+		-p 6080:6080 \
+		-p 5000:5000 \
+		-v /home/digital/RL:/root/RL \
+		-v /home/digital/rl_trainer:/root/rl_trainer \
+		-w /root \
+		--shm-size 16G \
+		--entrypoint=/bin/bash \
+		syuntoku/rl:ray
+		#--link mongo_db \
+		#--net sacred_omniboard \
+}
 
+run_docker_sacred() {
+	docker run -it --rm \
+		-p 6080:6080 \
+		-p 5000:5000 \
+		-v /home/digital/RL:/root/RL \
+		-v /home/digital/rl_trainer:/root/rl_trainer \
+		-w /root \
+		--link mongo_db \
+		--net sacred_omniboard \
+		--shm-size 16G \
+		--entrypoint=/bin/bash \
+		syuntoku/rl:ray
+}
 
+getopts "s" OPT
+case $OPT in
+	s ) 	echo "Support sacred"
+		run_docker_sacred ;;
+	? )	echo "No sacred"
+		run_docker ;;
+esac
--- syuntoku_rl/run_vnc.bash
+++ syuntoku_rl/run_vnc.bash
@@ -1,1 +1,2 @@
 /usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
+# vnc will show up on localhost:6080
