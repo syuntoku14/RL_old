c5d25ddeefb7f2465e5b2efcfc1435a3a8478ccd
syuntoku14 <syuntoku14@keio.jp>
2020-03-22 17:55:42 +0900

    Remove some files

--- docker_rl/run_tensorboard.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -eq 0 ]
-then
-	echo "No directory path supplied"
-else
-	docker run --rm -it -v $1:/logs -p 6006:6006 syuntoku/tensorboard
-fi
--- docker_rl/run_vnc.bash
+++
@@ -1,2 +0,0 @@
-/usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf
-# vnc will show up on localhost:6080
